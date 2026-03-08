7ac8e93627eee06a2376859fdf513a27475113b4
akanksha singh <akankshasinghmarko123@gmail.com>
2026-03-08 20:53:26 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const server = http.createServer((req,res) => {
+console.log(req.url,req.method);
+if(req.url==='/home') {
+  res.write('<h1> welcome to home </h1>');
+  return res.end();
+}else if (req.url==='/men ') {
+  res.write('<h1> welcome to men </h1>');
+  return res.end();
+}else if (req.url==='/women') {
+  res.write('<h1> welcome to    women </h1>');
+  return res.end();
+}else if (req.url==='/kid') {
+  res.write('<h1> welcome to kid </h1>');
+  return res.end();
+}       
+
+
+
+res.write(`<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>myntra</title>
+</head>
+<body>
+  <head>
+    <nav>
+      <ul>
+        <li><a href="home">Home</a></li>
+        <li><a href="men">Men</a></li>
+        <li><a href="women">Women</a></li>
+        <li><a href="kid">kids</a></li>
+      </ul>
+    </nav>
+  </head>
+  
+</body>
+</html>
+  `);
+  res.end();
+});
+  
+  server.listen(3001,()=>{
+  console.log('Server is listening on port  on http://localhost:3001');
+  });
